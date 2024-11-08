e0dbe052fc7892b2eb60aa996a351b36168fbcd0
Tobias <tobias.stuckenberger@gmail.com>
2024-11-08 13:51:57 +0100

    Help texts for stalls are done

--- src/config/stalls.js
+++ src/config/stalls.js
@@ -49,55 +49,62 @@ To avoid wait stalls, try increasing the number of active warps, restructuring t
     smsp__pcsamp_warps_issue_stalled_lg_throttle: {
         display_name: 'LG Throttle',
         format_function: formatStall,
-        help_text: 'Stall help',
+        help_text: `The warp was stalled waiting for the L1 instruction queue for local and global (LG) memory operations to be not full. Typically, this stall occurs only when executing local or global memory instructions extremely frequently.
+To avoid LG throttle stalls, avoid redundant global memory accesses. Also try to avoid using thread-local memory by checking if dynamically indexed arrays are declared in local scope, of if the kernel has excessive register pressure causing by spills. If applicable, consider combining multiple lower-width memory operations into fewer wider memory operations and try interleaving memory operations and math instructions.`,
         lower_better: true
     },
     smsp__pcsamp_warps_issue_stalled_selected: {
         display_name: 'Selected',
         format_function: formatStall,
-        help_text: 'Stall help',
+        help_text: `The warp was selected by the micro scheduler and issued an instruction.`,
         lower_better: true
     },
     smsp__pcsamp_warps_issue_stalled_not_selected: {
         display_name: 'Not Selected',
         format_function: formatStall,
-        help_text: 'Stall help',
+        help_text: `The warp was stalled waiting to be selected to fetch an instruction or waiting on an instruction cache miss. This happend when eligible warps were not picked by the warp scheduler to issue in the current cycle, as another warp was selected.
+High numbers of these stalls indicate there are a sufficient nmber of warps to cover warp latencies and the number of active warps could be reduced to possibly increase cache coherence and adta locality.`,
         lower_better: true
     },
     smsp__pcsamp_warps_issue_stalled_math_pipe_throttle: {
         display_name: 'Math Pipe Throttle',
         format_function: formatStall,
-        help_text: 'Stall help',
+        help_text: `The warp was stalled waiting for the execution pipe to be available. This stall occurs when a specific math pipeline gets oversaturated due to all active warps executing their next instruction on it.
+To avoid math pipe throttle stalls, the number of active warps should be increased to hide the existent latency. Changing the instructions used to utilize all available pipelines in a more balanced way can also help reduce these stalls.`,
         lower_better: true
     },
     smsp__pcsamp_warps_issue_stalled_dispatch_stall: {
         display_name: 'Dispatch',
         format_function: formatStall,
-        help_text: 'Stall help',
+        help_text: `The warp was stalled waiting on a dispatch stall. This happens when a warp has an instruction ready to issue, but the dispatcher holds back issuing the warp due to other conflicts or events.`,
         lower_better: true
     },
     smsp__pcsamp_warps_issue_stalled_mio_throttle: {
         display_name: 'MIO Throttle',
         format_function: formatStall,
-        help_text: 'Stall help',
+        help_text: `The warp was stalled waiting for the memory input/output (MIO) instruction queue to be not full. Reasons for a saturated MIO pipeline include extreme utilization of special math instructions, dynamic branching as well as shared memory instructions.
+To avoid mio throttle stalls, try to decrease utilization of these kinds of instructions. In the case of lots of shared memory accesses, using fewer but wider loads can reduce pipeline pressure.`,
         lower_better: true
     },
     smsp__pcsamp_warps_issue_stalled_tex_throttle: {
         display_name: 'TEX Throttle',
         format_function: formatStall,
-        help_text: 'Stall help',
+        help_text: `The warp was stalled waiting for the L1 instruction queue for texture operations to be not full. This means the L1TEX pipeline is saturated and utilized too much.
+To avoid tex throttle stalls, try issuing fewer texture fetches, surface loads, surface stores, or decoupled math operations. If applicable, consider combining multiple lower-width memory operations into fewer wider memory operations and try interleaving memory operations and math instructions. Consider converting texture lookups or surface loads into global memory lookups. Texture can accept four threads’ requests per cycle, whereas global accepts 32 threads.`,
         lower_better: true
     },
     smsp__pcsamp_warps_issue_stalled_drain: {
         display_name: 'Drain',
         format_function: formatStall,
-        help_text: 'Stall help',
+        help_text: `The warp was stalled after EXIT waiting for all outstanding memory operations to complete so that warp's resources can be freed. This typically occurs when a lot of data is written to memory towards the end of a kernel, when no or not many warps are left to hide this latency.
+To avoid drain stalls, make sure the memory access patterns of these store operations are optimal for the target architecture and consider parallelized data reduction, if applicable.`,
         lower_better: true
     },
     smsp__pcsamp_warps_issue_stalled_branch_resolving: {
         display_name: 'Branch Resolving',
         format_function: formatStall,
-        help_text: 'Stall help',
+        help_text: `The warp was stalled waiting for a branch target to be computed, and the warp program counter to be updated.
+To avoid branch resolving stalls, the number of jump/branch operations, as well as general control flow divergence should be reduced, for example by reducing or coalescing conditionals in the code.`,
         lower_better: true
     },
     undefined: { display_name: 'Undefined', format_function: formatStall, help_text: '', lower_better: true }
