d848670131317c73688a9fee8c194b0b4bbab9fd
Tobias Stuckenberger <tobias.stuckenberger@gmail.com>
2024-11-26 16:05:38 +0100

    Make Metrics in summary smaller

--- src/renderer/src/components/pages/summary/TopSectionSummary.vue
+++ src/renderer/src/components/pages/summary/TopSectionSummary.vue
@@ -1,41 +1,35 @@
 <template>
     <div class="flex h-full flex-row gap-2">
-        <MetricSection
-            :title="TEXT.analyses.general.warp_stall_analysis.title"
-            :hint="TEXT.analyses.general.warp_stall_analysis.hint"
-            :analysis-data="analysisData"
-            :comparison-analysis-data="comparisonAnalysisData"
-            :metrics="[
-                METRICS.stalls_total.name,
-                METRICS.stalls_short_scoreboard_perc.name,
-                METRICS.stalls_long_scoreboard_perc.name,
-                METRICS.stalls_imc_miss_perc.name,
-                METRICS.stalls_lg_throttle_perc.name,
-                METRICS.stalls_mio_throttle_perc.name,
-                METRICS.stalls_tex_throttle_perc.name
-            ]"
-            :values="[
-                (analysis, metric) => analysis.getMetric(metric),
-                (analysis, metric) => analysis.getMetric(metric),
-                (analysis, metric) => analysis.getMetric(metric),
-                (analysis, metric) => analysis.getMetric(metric),
-                (analysis, metric) => analysis.getMetric(metric),
-                (analysis, metric) => analysis.getMetric(metric),
-                (analysis, metric) => analysis.getMetric(metric)
-            ]"
-            :absolute-values="[(analysis) => analysis.getMetric(METRICS.stalls_total.name)]"
-            :expanded="true"
-        />
+        <div class="grid max-h-60 flex-grow grid-cols-3 gap-1 overflow-y-auto">
+            <ButtonMetricList
+                v-for="metric in metrics"
+                :key="metric"
+                :metric="metric"
+                :value="analysisData.getMetric(metric)"
+                :comparison-value="comparisonAnalysisData ? comparisonAnalysisData.getMetric(metric) : undefined"
+            />
+        </div>
     </div>
 </template>
 <script setup>
 import { METRICS } from '../../../../../config/metrics';
-import { TEXT } from '../../../../../config/text';
 import { Analysis } from '../../../utils/Analysis';
-import MetricSection from '../../ui/sections/MetricSection.vue';
+import ButtonMetricList from '../../ui/buttons/ButtonMetricList.vue';
 
 defineProps({
     analysisData: Analysis,
     comparisonAnalysisData: Analysis
 });
+
+const metrics = [
+    METRICS.stalls_total.name,
+    METRICS.stalls_short_scoreboard_perc.name,
+    METRICS.stalls_long_scoreboard_perc.name,
+    METRICS.stalls_imc_miss_perc.name,
+    METRICS.stalls_lg_throttle_perc.name,
+    METRICS.stalls_mio_throttle_perc.name,
+    METRICS.stalls_tex_throttle_perc.name,
+    METRICS.occupancy.name,
+    METRICS.general_total_instructions.name
+];
 </script>
