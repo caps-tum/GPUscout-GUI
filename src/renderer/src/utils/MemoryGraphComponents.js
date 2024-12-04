b3bca57b746ad226e272babc2585976befd3374f
Tobias <tobias.stuckenberger@gmail.com>
2024-12-04 14:33:55 +0100

    Add superclasses for memory graph stuff

--- src/renderer/src/utils/MemoryGraphComponents.js
+++ src/renderer/src/utils/MemoryGraphComponents.js
@@ -1,4 +1,8 @@
 export class MemoryGraph {
+    /**
+     * @param {Number} rows
+     * @param {...MemoryGraphElement} content
+     */
     constructor(rows, ...content) {
         this.rows = rows;
         this.content = content;
@@ -11,33 +15,59 @@ export class MemoryGraph {
     }
 }
 
-export class Node {
-    constructor(...content) {
-        this.content = content;
-        this.rowSpan = 1;
-        this.spaceAbove = false;
+class NodeContent {
+    constructor() {
+        this.bold = false;
     }
+}
 
-    setRowSpan(rowSpan) {
-        this.rowSpan = rowSpan;
-        return this;
+class MemoryGraphElement {
+    constructor() {
+        this.spaceAbove = false;
+        this.spaceBelow = false;
     }
 
     addSpaceAbove() {
         this.spaceAbove = true;
         return this;
     }
+
+    addSpaceBelow() {
+        this.spaceBelow = true;
+        return this;
+    }
 }
 
-export class NodeTextContent {
+export class Node extends MemoryGraphElement {
+    /**
+     * @param {...NodeContent} content
+     */
+    constructor(...content) {
+        super();
+        this.content = content;
+        this.rowSpan = 1;
+    }
+
+    /**
+     * @param {Number} rowSpan
+     */
+    setRowSpan(rowSpan) {
+        this.rowSpan = rowSpan;
+        return this;
+    }
+}
+
+export class NodeTextContent extends NodeContent {
     constructor(title, bold = true) {
+        super();
         this.title = title;
         this.bold = bold;
     }
 }
 
-export class NodeMetricContent {
+export class NodeMetricContent extends NodeContent {
     constructor(metric, format = '{value}', comparisonFormat = '{value} vs {comp_value}') {
+        super();
         this.metric = metric;
         this.format = format;
         this.comparisonFormat = comparisonFormat;
@@ -52,29 +82,18 @@ export const DIRECTION = {
     BOTH: 4
 };
 
-export class Arrow {
+export class Arrow extends MemoryGraphElement {
     constructor(metric, metricBottom = undefined) {
+        super();
         this.metric = metric;
         this.metricBottom = metricBottom;
         this.direction = DIRECTION.RIGHT;
-        this.spaceAbove = false;
-        this.spaceBelow = false;
     }
 
     setDirection(direction) {
         this.direction = direction;
         return this;
     }
-
-    addSpaceAbove() {
-        this.spaceAbove = true;
-        return this;
-    }
-
-    addSpaceBelow() {
-        this.spaceBelow = true;
-        return this;
-    }
 }
 
-export class Spacer {}
+export class Spacer extends MemoryGraphElement {}
