2f1000e9caae4c9e1ad87e441a80b30bd7f3eb0a
Tobias <tobias.stuckenberger@gmail.com>
2025-01-15 12:11:19 +0100

    Reset selected line on source file change

--- src/renderer/src/stores/CodeViewerStore.js
+++ src/renderer/src/stores/CodeViewerStore.js
@@ -92,6 +92,7 @@ export const useCodeViewerStore = defineStore('codeViewer', () => {
 
     function setCurrentSourceFile(fileName) {
         currentSourceFile.value = fileName;
+        setSelectedLine(selectedLine.value);
     }
 
     /**
